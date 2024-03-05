cbfd78b4d3768482c496f2f5855749b2e6b3b29c
Giovani Rizzato <giovanirizzato@gmail.com>
2024-03-05 15:56:04 -0300

    Fixing the environment proxy problem

--- src/app/movie-service/movie.service.spec.ts
+++ src/app/movie-service/movie.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
 import { MovieService } from './movie.service';
 
 describe('MovieService', () => {
@@ -25,31 +26,31 @@ describe('MovieService', () => {
 
   it('should get all movies list', () => {
     service.getAllMovies(9, 99, true, 2018).subscribe(() => {});
-    const req = httpMock.expectOne('https://tools.texoit.com/backend-java/api/movies?page=9&size=99&winner=true&year=2018');
+    const req = httpMock.expectOne(`${environment.apiUrl}?page=9&size=99&winner=true&year=2018`);
     expect(req.request.method).toBe('GET');
   });
 
   it('should get the year winning count list', () => {
     service.getYearsWithMultipleWinners().subscribe(() => {});
-    const req = httpMock.expectOne('https://tools.texoit.com/backend-java/api/movies?projection=years-with-multiple-winners');
+    const req = httpMock.expectOne(`${environment.apiUrl}?projection=years-with-multiple-winners`);
     expect(req.request.method).toBe('GET');
   });
 
   it('should getStudiosWithWinCount', () => {
     service.getStudiosWithWinCount().subscribe(() => {});
-    const req = httpMock.expectOne('https://tools.texoit.com/backend-java/api/movies?projection=studios-with-win-count');
+    const req = httpMock.expectOne(`${environment.apiUrl}?projection=studios-with-win-count`);
     expect(req.request.method).toBe('GET');
   });
 
   it('should getWinIntervalForProducers', () => {
     service.getWinIntervalForProducers().subscribe(() => {});
-    const req = httpMock.expectOne('https://tools.texoit.com/backend-java/api/movies?projection=max-min-win-interval-for-producers');
+    const req = httpMock.expectOne(`${environment.apiUrl}?projection=max-min-win-interval-for-producers`);
     expect(req.request.method).toBe('GET');
   });
 
   it('should getWinnersByYear', () => {
     service.getWinnersByYear(true, 1993).subscribe(() => {});
-    const req = httpMock.expectOne('https://tools.texoit.com/backend-java/api/movies?winner=true&year=1993');
+    const req = httpMock.expectOne(`${environment.apiUrl}?winner=true&year=1993`);
     expect(req.request.method).toBe('GET');
   });
 });
--- src/app/movie-service/movie.service.ts
+++ src/app/movie-service/movie.service.ts
@@ -2,38 +2,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MinAndMaxWinIntervalForProducers, Movie, StudioWinningCountList, YearWinningCountList } from './movie-models';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  //private apiUrl = 'https://tools.texoit.com/backend-java/api/movies';
-  private apiUrl = 'http://localhost:3100/api/movies';
-
   constructor(private http: HttpClient) { }
 
   // Método para obter a lista de todos os filmes
   getAllMovies(page: number, size: number, isWinner: boolean, year: number): Observable<any> {
-    return this.http.get(`${this.apiUrl}?page=${page}&size=${size}&winner=${isWinner}&year=${year}`);
+    return this.http.get(`${environment.apiUrl}?page=${page}&size=${size}&winner=${isWinner}&year=${year}`);
   }
 
   // Método para obter os anos com mais de um vencedor
   getYearsWithMultipleWinners(): Observable<YearWinningCountList> {
-    return this.http.get<YearWinningCountList>(`${this.apiUrl}?projection=years-with-multiple-winners`);
+    return this.http.get<YearWinningCountList>(`${environment.apiUrl}?projection=years-with-multiple-winners`);
   }
 
   // Método para obter os estúdios com contagem de vitórias
   getStudiosWithWinCount(): Observable<StudioWinningCountList> {
-    return this.http.get<StudioWinningCountList>(`${this.apiUrl}?projection=studios-with-win-count`);
+    return this.http.get<StudioWinningCountList>(`${environment.apiUrl}?projection=studios-with-win-count`);
   }
 
   // Método para obter os intervalos de vitórias dos produtores
   getWinIntervalForProducers(): Observable<MinAndMaxWinIntervalForProducers> {
-    return this.http.get<MinAndMaxWinIntervalForProducers>(`${this.apiUrl}?projection=max-min-win-interval-for-producers`);
+    return this.http.get<MinAndMaxWinIntervalForProducers>(`${environment.apiUrl}?projection=max-min-win-interval-for-producers`);
   }
 
   // Método para obter os vencedores de determinado ano
   getWinnersByYear(isWinner: boolean, year: number): Observable<Movie> {
-    return this.http.get<Movie>(`${this.apiUrl}?winner=${isWinner}&year=${year}`);
+    return this.http.get<Movie>(`${environment.apiUrl}?winner=${isWinner}&year=${year}`);
   }
 }
